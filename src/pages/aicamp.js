4ad467bc7559ca8a6e50960023714c8b201071c4
Devin Jean <emailcruzjean@yahoo.com>
2025-04-03 08:40:22 -0500

    componentize aicamp

--- src/pages/aicamp.js
+++ src/pages/aicamp.js
@@ -3,70 +3,13 @@ import { Brain, Bot, Gamepad, MessageSquare, Code, ExternalLink, GraduationCap, 
 
 import {Page, Card, Warning} from "../components.js";
 
-const projects = [
-  {
-    title: "Ball-Balancing Reinforcement Learning Agent",
-    description: 'In this project, we provide students a starter project that implements a reinforcement learning (RL) agent tasked with balancing a ball on a beam without letting it roll off either side. Students are challenged to come up with a "reward function" that quickly/efficiently teaches the RL agent what kinds of behaviors are good/bad in order to learn.',
-    icon: Brain,
-    starterUrl: "https://editor.netsblox.org/?action=present&Username=devinjean&ProjectName=ball-balancing-starter&editMode&noRun",
-    completedUrl: "https://editor.netsblox.org/?action=present&Username=devinjean&ProjectName=ball-balancing&editMode&noRun",
-    note: null,
-  },
-  {
-    title: "Minecraft Maze Reinforcement Learning Agent",
-    description: "In this (more difficult) RL project, students must come up with a reward function that can teach a 2D character how to explore a maze and find diamonds. Students are encouraged to create reward functions that do not assume anything about the maze (i.e., the reward function should not include the distance to diamonds or any other information that a player should not be able to know).",
-    icon: Gamepad,
-    starterUrl: "https://editor.netsblox.org/?action=present&Username=devinjean&ProjectName=minecraft-maze-starter&editMode&noRun",
-    completedUrl: "https://editor.netsblox.org/?action=present&Username=devinjean&ProjectName=minecraft-maze&editMode&noRun",
-    note: null,
-  },
-  {
-    title: "Turing Test Activity",
-    description: "In this project, students join a NetsBlox distributed chatroom and are randomly paired either with another student or with an instance of ChatGPT. Students are then put into a time- and round-based back and forth dialog and are tasked with determining whether the other person is a human or a computer.",
-    icon: MessageSquare,
-    starterUrl: null,
-    completedUrl: [
-      ["Completed Client", "https://editor.netsblox.org/?extensions=%5B%22https%3A%2F%2Fextensions.netsblox.org%2Fextensions%2FOpenAIBlocks%2Findex.js%22%5D&action=present&Username=devinjean&ProjectName=turing-test-client&editMode&noRun"],
-      ["Completed Server", "https://editor.netsblox.org/?extensions=%5B%22https%3A%2F%2Fextensions.netsblox.org%2Fextensions%2FOpenAIBlocks%2Findex.js%22%5D&action=present&Username=devinjean&ProjectName=turing-test-server&editMode&noRun"],
-    ],
-    note: "Whoever runs the server will need to have an OpenAI API key in order to use the OpenAI blocks provided by the extension. To add your key, use the puzzle-shaped Extensions menu near the top right of the NetsBlox editor. The API key is saved in local storage, so only do this on your personal computer!",
-  },
-];
-
-const ProjectCard = ({ project }) => (
-  <div className="bg-white/50 dark:bg-slate-800/50 backdrop-blur-sm p-4 rounded-lg">
-    <div className="flex items-center gap-3 mb-4">
-      <div className="p-2 rounded-full bg-purple-100 dark:bg-purple-900">
-        <project.icon className="h-6 w-6 text-purple-600 dark:text-purple-400" />
-      </div>
-      <h3 className="text-lg font-bold">{project.title}</h3>
-    </div>
-    <p className="text-slate-600 dark:text-slate-300 mb-4 text-justify">
-      {project.description}
-    </p>
-    <div className="flex gap-2">
-      {project.starterUrl &&
-        <a href={project.starterUrl} className="flex items-center gap-2 border border-gray-300 rounded px-2 py-1">
-          <BookOpen className="h-4 w-4" />
-          Starter Project
-        </a>
-      }
-      {project.completedUrl && (Array.isArray(project.completedUrl) ? project.completedUrl : [["Completed Project", project.completedUrl]]).map(meta =>
-        <a href={meta[1]} className="flex items-center gap-2 border border-gray-300 rounded px-2 py-1">
-          <BookOpenCheck className="h-4 w-4" />
-          {meta[0]}
-        </a>
-      )}
-    </div>
-    {project.note && <Warning>Note: {project.note}</Warning>}
-  </div>
-);
+const color = "text-purple-600 dark:text-purple-40";
 
 const ExternalLinks = infos => infos.map(info => <li><a href={info[1]}><ExternalLink className="h-4 w-4 mr-3 inline-block"/>{info[0]}</a>{info[2] && <><br/><em>{info[2]}</em></>}</li>);
 
 export default () => (
   <Page>
-    <h1><Bot className="h-12 w-12 mr-3 text-purple-600 inline-block"/>AI Camp Curriculum</h1>
+    <h1><Bot className={`h-12 w-12 mr-3 inline-block ${color}`}/>AI Camp Curriculum</h1>
 
     <p>
       This page provides resources for our NetsBlox-based AI summer camp curriculum. Over the course of this module, students are introduced to some of the earliest history of AI, a few "classical" AI algorithms, and finally some of the most recent breakthroughs in AI, such as the Large Language Models that power ChatGPT. However, we won't only be learning these topics: we'll be building many of them in NetsBlox!
@@ -74,44 +17,61 @@ export default () => (
 
     <h2>Materials</h2>
 
-    <p>
-      <GraduationCap className="h-6 w-6 text-purple-600 inline-block"/> <a href="https://docs.google.com/document/d/1cYXk0e5gJvPNNAK7hVaYL92rY6LBhiG1/edit?usp=sharing&ouid=109658049399807181387&rtpof=true&sd=true">Syllabus</a> and <a href="https://docs.google.com/document/d/18NAtN2zkJ43KiPlO2KIB5-Pv-Uab_mLK/edit?usp=sharing&ouid=109658049399807181387&rtpof=true&sd=true">Lesson Plan</a>
-    </p>
-
-    <p>
-      <BookOpen className="h-6 w-6 text-purple-600 inline-block"/> Slides for {[
-        "https://docs.google.com/presentation/d/188nltVxQSKqMbygC-cd1nB52rxQriuf4nzLJg2cUnRw/edit?usp=sharing",
-        "https://docs.google.com/presentation/d/1z3IEMEtK6uhHfu40vUL1Cjno3X9erOQKx7-owggN-ZU/edit?usp=sharing",
-        "https://docs.google.com/presentation/d/1Pf9qvz1UVUmnt87XwkeZcpm_RYAV4NMebHIW9Jk63kk/edit?usp=sharing",
-        "https://docs.google.com/presentation/d/1BliPIzu7qy2ejdJTajxnuXhLtopXgeMgRt4c0DtwXMg/edit?usp=sharing",
-      ].map((url, i) => <><a href={url}>Day {i + 1}</a>{i < 2 ? ", " : i < 3 ? ", and " : ""}</>)}
-    </p>
+    <ul>
+      <li>
+        <GraduationCap className={`h-6 w-6 mr-3 inline-block ${color}`}/> <a href="https://docs.google.com/document/d/1cYXk0e5gJvPNNAK7hVaYL92rY6LBhiG1/edit?usp=sharing&ouid=109658049399807181387&rtpof=true&sd=true">Syllabus</a> and <a href="https://docs.google.com/document/d/18NAtN2zkJ43KiPlO2KIB5-Pv-Uab_mLK/edit?usp=sharing&ouid=109658049399807181387&rtpof=true&sd=true">Lesson Plan</a>
+      </li>
+      <li>
+        <BookOpen className={`h-6 w-6 mr-3 inline-block ${color}`}/> Slides for {[
+          "https://docs.google.com/presentation/d/188nltVxQSKqMbygC-cd1nB52rxQriuf4nzLJg2cUnRw/edit?usp=sharing",
+          "https://docs.google.com/presentation/d/1z3IEMEtK6uhHfu40vUL1Cjno3X9erOQKx7-owggN-ZU/edit?usp=sharing",
+          "https://docs.google.com/presentation/d/1Pf9qvz1UVUmnt87XwkeZcpm_RYAV4NMebHIW9Jk63kk/edit?usp=sharing",
+          "https://docs.google.com/presentation/d/1BliPIzu7qy2ejdJTajxnuXhLtopXgeMgRt4c0DtwXMg/edit?usp=sharing",
+        ].map((url, i) => <><a href={url}>Day {i + 1}</a>{i < 2 ? ", " : i < 3 ? ", and " : ""}</>)}
+      </li>
+    </ul>
 
     <h2>Projects</h2>
 
-    {projects.map((project, index) => <ProjectCard key={index} project={project} />)}
-
     <Card>
-      <h2><Brain className="h-5 w-5 mr-3 text-purple-600 inline-block"/> AI Topic Exploration</h2>
-      <ul>
-        {ExternalLinks([
-          ["But what is a neural network?", "https://www.youtube.com/watch?v=aircAruvnKk", null],
-          ["Gradient descent, how neural networks learn", "https://www.youtube.com/watch?v=IHZwWFHWa-w", null],
-          ["What is backpropagation really doing?", "https://www.youtube.com/watch?v=Ilg3gGewQ5U", null],
-          ["Backpropagation calculus", "https://www.youtube.com/watch?v=tIeHLnjs5U8", null],
-          ["But what is a GPT? Visual intro to transformers", "https://www.youtube.com/watch?v=wjZofJX0v4M", null],
-          ["Attention in transformers, visually explained", "https://www.youtube.com/watch?v=eMlx5fFNoYc", null],
-        ])}
-      </ul>
+      <h3><Brain className={`h-6 w-6 mr-3 inline-block ${color}`}/>Ball-Balancing Reinforcement Learning Agent</h3>
+      <p>In this project, we provide students a starter project that implements a reinforcement learning (RL) agent tasked with balancing a ball on a beam without letting it roll off either side. Students are challenged to come up with a "reward function" that quickly/efficiently teaches the RL agent what kinds of behaviors are good/bad in order to learn.</p>
+      <a href="https://editor.netsblox.org/?action=present&Username=devinjean&ProjectName=ball-balancing-starter&editMode&noRun"><button><BookOpen className="h-4 w-4 mr-3 inline-block"/>Starter Project</button></a>
+      <a href="https://editor.netsblox.org/?action=present&Username=devinjean&ProjectName=ball-balancing&editMode&noRun"><button><BookOpenCheck className="h-4 w-4 mr-3 inline-block"/>Completed Project</button></a>
     </Card>
 
     <Card>
-      <h2><Code className="h-5 w-5 mr-3 text-purple-600 inline-block"/>AI Coding in Python</h2>
-      <ul>
-        {ExternalLinks([
-          ["Q-Learning in Python", "https://www.geeksforgeeks.org/q-learning-in-python/", "This is the algorithm we implemented for you in our RL projects"],
-        ])}
-      </ul>
+      <h3><Gamepad className={`h-6 w-6 mr-3 inline-block ${color}`}/>Minecraft Maze Reinforcement Learning Agent</h3>
+      <p>In this (more difficult) RL project, students must come up with a reward function that can teach a 2D character how to explore a maze and find diamonds. Students are encouraged to create reward functions that do not assume anything about the maze (i.e., the reward function should not include the distance to diamonds or any other information that a player should not be able to know).</p>
+      <a href="https://editor.netsblox.org/?action=present&Username=devinjean&ProjectName=minecraft-maze-starter&editMode&noRun"><button><BookOpen className="h-4 w-4 mr-3 inline-block"/>Starter Project</button></a>
+      <a href="https://editor.netsblox.org/?action=present&Username=devinjean&ProjectName=minecraft-maze&editMode&noRun"><button><BookOpenCheck className="h-4 w-4 mr-3 inline-block"/>Completed Project</button></a>
     </Card>
+
+    <Card>
+      <h3><MessageSquare className={`h-6 w-6 mr-3 inline-block ${color}`}/>Turing Test Activity</h3>
+      <p>In this project, students join a NetsBlox distributed chatroom and are randomly paired either with another student or with an instance of ChatGPT. Students are then put into a time- and round-based back and forth dialog and are tasked with determining whether the other person is a human or a computer.</p>
+      <a href="https://editor.netsblox.org/?extensions=%5B%22https%3A%2F%2Fextensions.netsblox.org%2Fextensions%2FOpenAIBlocks%2Findex.js%22%5D&action=present&Username=devinjean&ProjectName=turing-test-client&editMode&noRun"><button><BookOpenCheck className="h-4 w-4 mr-3 inline-block"/>Completed Client</button></a>
+      <a href="https://editor.netsblox.org/?extensions=%5B%22https%3A%2F%2Fextensions.netsblox.org%2Fextensions%2FOpenAIBlocks%2Findex.js%22%5D&action=present&Username=devinjean&ProjectName=turing-test-server&editMode&noRun"><button><BookOpenCheck className="h-4 w-4 mr-3 inline-block"/>Completed Server</button></a>
+      <Warning>Note: Whoever runs the server will need to have an OpenAI API key in order to use the OpenAI blocks provided by the extension. To add your key, use the puzzle-shaped Extensions menu near the top right of the NetsBlox editor. The API key is saved in local storage, so only do this on your personal computer!</Warning>
+    </Card>
+
+    <h2><Brain className={`h-5 w-5 mr-3 inline-block ${color}`}/>AI Topic Exploration</h2>
+    <ul>
+      {ExternalLinks([
+        ["But what is a neural network?", "https://www.youtube.com/watch?v=aircAruvnKk", null],
+        ["Gradient descent, how neural networks learn", "https://www.youtube.com/watch?v=IHZwWFHWa-w", null],
+        ["What is backpropagation really doing?", "https://www.youtube.com/watch?v=Ilg3gGewQ5U", null],
+        ["Backpropagation calculus", "https://www.youtube.com/watch?v=tIeHLnjs5U8", null],
+        ["But what is a GPT? Visual intro to transformers", "https://www.youtube.com/watch?v=wjZofJX0v4M", null],
+        ["Attention in transformers, visually explained", "https://www.youtube.com/watch?v=eMlx5fFNoYc", null],
+      ])}
+    </ul>
+
+    <h2><Code className={`h-5 w-5 mr-3 inline-block ${color}`}/>AI Coding in Python</h2>
+    <ul>
+      {ExternalLinks([
+        ["Q-Learning in Python", "https://www.geeksforgeeks.org/q-learning-in-python/", "This is the algorithm we implemented for you in our RL projects"],
+      ])}
+    </ul>
   </Page>
 );
--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,23 +1,17 @@
 import React from "react";
 
-const generateLinks = links => links.map(([text, url]) => <li><a href={url} className="hover:text-blue-500 dark:hover:text-blue-400 transition-colors">{text}</a></li>);
+const generateLinks = links => links.map(([text, url]) => <li><a href={url}>{text}</a></li>);
 
 export default () => (
-  <footer className="bg-white dark:bg-gray-900 text-gray-600 dark:text-gray-300 py-8">
+  <footer className="bg-white dark:bg-gray-900 text-gray-600 dark:text-gray-300 py-4">
     <div className="container mx-auto px-4">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div>
-          <h3 className="text-xl font-semibold mb-4 text-blue-500 dark:text-blue-400">
-            About NetsBlox
-          </h3>
-          <p>
-            NetsBlox is a project of the Institute for Software Integrated Systems at Vanderbilt University.
-          </p>
+          <h3>About NetsBlox</h3>
+          <p>NetsBlox is a project of the Institute for Software Integrated Systems at Vanderbilt University.</p>
         </div>
         <div>
-          <h3 className="text-xl font-semibold mb-4 text-blue-500 dark:text-blue-400">
-            Additional Resources
-          </h3>
+          <h3>Additional Resources</h3>
           <ul className="space-y-2">
             {generateLinks([
               ["Research", "research"],
@@ -25,9 +19,7 @@ export default () => (
           </ul>
         </div>
         <div>
-          <h3 className="text-xl font-semibold mb-4 text-blue-500 dark:text-blue-400">
-            Connect
-          </h3>
+          <h3>Connect</h3>
           <ul className="space-y-2">
             {generateLinks([
               ["Contact Us", "contact"],
@@ -37,9 +29,7 @@ export default () => (
           </ul>
         </div>
       </div>
-      <div className="mt-8 text-center">
-        <p>&copy; 2025 Institute for Software Integrated Systems / Vanderbilt University / School of Engineering</p>
-      </div>
+      <p className="text-center">&copy; 2025 Institute for Software Integrated Systems / Vanderbilt University / School of Engineering</p>
     </div>
   </footer>
 );
